7a76bba76f9b6a7ba96ff163ce2bfc7510d42494
chetan-bist <bistc3712@gmail.com>
2026-07-11 21:11:25 +0545

    update round 3 match result

--- data/topgoalscorer.ts
+++ data/topgoalscorer.ts
@@ -10,16 +10,23 @@ export const topgoalscorer: Topgoalscorer[] = [
     position: 1,
     teamName: 'Sagarmatha FC',
     playerName: 'Gyanendra Joshi',
-    goals: 5,
+    goals: 9,
   },
+  
   {
     position: 2,
+    teamName: 'Blue Lock FC',
+    playerName: 'Anuj Khadka',
+    goals: 7,
+  },
+  {
+    position: 3,
     teamName: 'Bhamka FC',
     playerName: 'Nipurna Chand',
     goals: 5,
   },
   {
-    position: 3,
+    position: 4,
     teamName: 'Ghatal Baba FC',
     playerName: 'Shirish Dhami',
     goals: 4,
@@ -28,43 +35,50 @@ export const topgoalscorer: Topgoalscorer[] = [
   {
     position: 5,
     teamName: 'Blue Lock FC',
-    playerName: 'Anuj Khadka',
-    goals: 3,
-  },
-  {
-    position: 6,
-    teamName: 'Blue Lock FC',
     playerName: 'Gajendra Bist',
     goals: 2,
   },
   {
-    position: 7,
+    position: 6,
     teamName: 'Ghatal Baba FC',
     playerName: 'Awash',
     goals: 2,
   },
   {
-    position: 8,
+    position: 7,
     teamName: 'Tiger Land FC',
     playerName: 'Harish Dhami',
     goals: 1,
   },
   {
-    position: 9,
+    position: 8,
     teamName: 'Ghatal Baba FC',
     playerName: 'Rohit',
     goals: 1,
   },
   {
-    position: 10,
+    position: 9,
     teamName: 'Ghatal Baba FC',
     playerName: 'Harish',
     goals: 1,
   },
   {
-    position: 11,
+    position: 10,
     teamName: 'Iron Beast FC',
     playerName: 'Sonu',
     goals: 1,
   },
+  {
+    position: 11,
+    teamName: 'Tiger Land FC',
+    playerName: 'Sakira',
+    goals: 1,
+  },
+  {
+    position: 12,
+    teamName: 'Bhamka FC',
+    playerName: 'Sameer',
+    goals: 1,
+  },
+
 ]
